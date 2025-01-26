40cac985ed7507cc73425147b5389e8486fab0bd
Mariyamsalman <salmanmariyam47@gmail.com>
2025-01-26 18:28:07 +0500

    done by maryam

--- pf final project1.cpp
+++ pf final project1.cpp
@@ -61,3 +61,34 @@ void displayStudent(const Student& student) {
 void displayCourse(const Course& course) {
     cout << "Course ID: " << course.courseId << ", Course Name: " << course.courseName << ", Instructor: " << course.instructor << endl;
 }
+void saveStudentsToFile() {
+    ofstream outFile("students.txt");
+    if (!outFile) {
+        cout << "Error: Unable to save students to file.\n";
+        return;
+    }
+    for (int i = 0; i < studentCount; ++i) {
+        outFile << students[i].id << "\n" << students[i].name << "\n" << students[i].department << "\n";
+    }
+    outFile.close();
+}
+
+void loadStudentsFromFile() {
+    ifstream inFile("students.txt");
+    if (!inFile) {
+        cout << "Note: No existing student data found. Starting fresh.\n";
+        return;
+    }
+    studentCount = 0;
+    while (inFile) {
+        Student s;
+        inFile >> s.id;
+        inFile.ignore();  // Ignore newline after reading id
+        if (inFile.eof()) break;
+        getline(inFile, s.name);
+        getline(inFile, s.department);
+        students[studentCount++] = s;
+    }
+    inFile.close();
+}
+
